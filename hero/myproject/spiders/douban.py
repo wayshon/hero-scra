5e0db39e9027b15625f71d23d94d2c5680b1a534
wx <wangxu08324@hellobike.com>
2019-06-04 16:33:45 +0800

    tag 和 多页

--- hero/myproject/spiders/douban.py
+++ hero/myproject/spiders/douban.py
@@ -8,29 +8,41 @@ class DoubanSpider(scrapy.Spider):
     allowed_domains = ['douban.com']
     # start_urls = ['https://www.douban.com/group/shanghaizufang/discussion?start=0']
 
+    # 另外一种初始链接写法
     def start_requests(self): # 由此方法通过下面链接爬取页面
-        
-        # 定义爬取的链接
-        urls = [
-            'https://www.douban.com/group/shanghaizufang/discussion?start=0',
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse) #爬取到的页面如何处理？提交给parse方法处理
+        group = getattr(self, 'group', 'shanghaizufang')  # 获取tag值，也就是爬取时传过来的参数
 
+        url = 'https://www.douban.com/group/%s/discussion?start=0' % group
+        yield scrapy.Request(url=url, callback=self.parse) #爬取到的页面如何处理？提交给parse方法处理
+            
+
+    # 如果是简写初始url，此方法名必须为：parse
     def parse(self, response):
         results = []
         trs = response.css('table.olt tr')
         for tr in trs:
             title = tr.css('td.title a::attr(title)').extract_first()
             href = tr.css('td.title a::attr(href)').extract_first()
+            comment = tr.css('td[nowrap="nowrap"][class=""]::text').extract_first()
+            username = tr.css('td[nowrap="nowrap"] a::text').extract_first()
+            userlink = tr.css('td[nowrap="nowrap"] a::attr(href)').extract_first()
             upodatetime = tr.css('td.time::text').extract_first()
+            
+            commentNum = 0
+            if (comment):
+                commentNum = int(comment)
             # yield {'title': title, 'href': href, 'upodatetime': upodatetime}
-            results.append({'title': title, 'href': href, 'upodatetime': upodatetime})
+            results.append({'title': title, 'href': href, 'username': username, 'userlink': userlink, 'comment': commentNum, 'upodatetime': upodatetime})
 
-        with open('douban.json', 'w', encoding='utf-8') as f:
+        with open('douban.json', 'a', encoding='utf-8') as f:
             json.dump(results, f, ensure_ascii=False)
 
         self.log('保存文件: 成功')
+
+        next_page = response.css('div.paginator > span.next a::attr(href)').extract_first()
+        if next_page is not None: 
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
 
         
         
