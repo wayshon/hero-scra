71ed2bc368fcbf4fde79d4c8cbde5b31af33039d
wx <wangxu08324@hellobike.com>
2019-06-04 15:13:03 +0800

    试试

--- hero/myproject/spiders/douban.py
+++ hero/myproject/spiders/douban.py
@@ -1,27 +1,36 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
-# class CellItem(scrapy.Item):
-#     title = scrapy.Field()
-#     href = scrapy.Field()
-#     author = scrapy.Field()
-#     upodatetime = scrapy.Field()
+import json
 
 class DoubanSpider(scrapy.Spider):
     name = 'douban'
     allowed_domains = ['douban.com']
-    start_urls = ['https://www.douban.com/group/shanghaizufang/discussion?start=0']
+    # start_urls = ['https://www.douban.com/group/shanghaizufang/discussion?start=0']
+
+    def start_requests(self): # 由此方法通过下面链接爬取页面
+        
+        # 定义爬取的链接
+        urls = [
+            'https://www.douban.com/group/shanghaizufang/discussion?start=0',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse) #爬取到的页面如何处理？提交给parse方法处理
 
     def parse(self, response):
+        results = []
         trs = response.css('table.olt tr')
         for tr in trs:
             title = tr.css('td.title a::attr(title)').extract_first()
             href = tr.css('td.title a::attr(href)').extract_first()
             upodatetime = tr.css('td.time::text').extract_first()
-            yield {'title': title, 'href': href, 'upodatetime': upodatetime}
+            # yield {'title': title, 'href': href, 'upodatetime': upodatetime}
+            results.append({'title': title, 'href': href, 'upodatetime': upodatetime})
 
-        # a = response.css('a.j.a_show_login.bn-join-group > span::text').extract_first()
+        with open('douban.json', 'w', encoding='utf-8') as f:
+            json.dump(results, f, ensure_ascii=False)
 
-        # a = response.css('table.olt td.title a::attr(title)').extract_first()
-        # yield {'title': a, 'href': a}
+        self.log('保存文件: 成功')
+
+        
         
